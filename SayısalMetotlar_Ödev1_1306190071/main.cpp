720c5d6f599e21c404e313b05109f67092f56fef
korayercan <75748515+korayercan@users.noreply.github.com>
2022-03-02 21:35:14 +0300

    Ödev1

---
+++ SayısalMetotlar_Ödev1_1306190071/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+float f(float x){
+    return x*x*x-7*x*x+14*x-6;
+}
+void yarilama_metodu(){
+    int i = 1;
+
+
+}
+
+
+int main()
+{
+
+
+    return 0;
+}
